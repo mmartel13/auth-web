bcb82993270d1c9b06d6630f360d981fcc590282
Megan Martel <mmartel1330@gmail.com>
2022-03-01 11:22:12 -0500

    login and signup scenes built

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Signup from './scenes/Signup';
+import Login from './scenes/Login';
 import './App.css';
 
+
 function App() {
+  const [token, setToken] = useState();
+  const [isUser, setIsUser] = useState(); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      {!token
+          ? isUser //if is a user show login, if not show sign up. Nested ternary. 
+             ? <Login setToken={setToken} setIsUser={setIsUser} />
+             : <Signup setToken={setToken} setIsUser={setIsUser} />
+          : <h1>User List</h1>
+      }
+    </section>
   );
 }
 
---
+++ src/scenes/Login.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+export default function Login({ setToken, setIsUser }) {//need set token, bc when user logs in we need to set their token
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState(''); 
+    const handleSubmit = (e) => {
+        e.preventDefault() //when you submit the form you don't want it to default reload the same form. We want it to do another action
+        fetch('http://localhost:3001/users',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        })
+        .then(response => response.json())
+        .then(data => setToken(data.token))
+        .catch(alert)
+    }
+    return (
+        <>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Email:
+                    <input type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+                </label><br />
+                <label>Password:
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                </label><br />
+                    <input type="submit" value="Login" />
+            </form>
+            <button onClick={() => setIsUser(false)}>Signup</button>
+        </>
+    )
+}
